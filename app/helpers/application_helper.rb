9503a7a9473195c6f0b007c1cd53804e69157f51
Adam Krebs <amk528@cs.nyu.edu>
2013-11-12 01:45:55 -0500

    use the most recent event model as the helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,14 +9,8 @@ module ApplicationHelper
     }[flash_type] || flash_type.to_s
   end
 
-  def next_event_info
-    {
-      title: 'Asset Mapping For Artists',
-      date: 'Tuesday, December 17th',
-      time: '6:30-8:30pm',
-      venue: 'New York Live Arts',
-      address: '219 W 19th St (btw 7th and 8th Aves)',
-    }
+  def event
+    # Always the most recent event
+    Event.last
   end
-
 end
