fa2b37232f06e68b79c905e8debcd6bce6c6cff0
Adam Krebs <amk528@cs.nyu.edu>
2013-11-12 01:49:29 -0500

    fix date on original signup, actually persist signups to the server now

--- db/migrate/20131112062302_create_events.rb
+++ db/migrate/20131112062302_create_events.rb
@@ -14,7 +14,7 @@ class CreateEvents < ActiveRecord::Migration
 
     event = Event.create({
       title: 'Asset Mapping For Artists',
-      date: 'Tuesday, December 17th',
+      date: 'Thursday, November 7th',
       time: '6:30-8:30pm',
       venue: 'New York Live Arts',
       address: '219 W 19th St (btw 7th and 8th Aves)',
@@ -23,5 +23,13 @@ class CreateEvents < ActiveRecord::Migration
     Signup.all.each do |signup|
       signup.update_attribute(:event_id, event.id)
     end
+
+    Event.create({
+      title: 'Asset Mapping For Artists',
+      date: 'Tuesday, December 17th',
+      time: '6:30-8:30pm',
+      venue: 'New York Live Arts',
+      address: '219 W 19th St (btw 7th and 8th Aves)',
+    })
   end
 end
