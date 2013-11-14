49f6a213b1dec044b875e4df28806c0a75166195
Adam Krebs <amk528@cs.nyu.edu>
2013-11-14 14:00:11 -0500

    use env for secret key if not set

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-OurgoodsSplash::Application.config.secret_key_base = '423b7172dcd2a216dc52fe46f13d618d66c79129f256afd81d6b31fb68ca526a005576a36e9ce4d110666667e2ca404ae760bbe04511d14020cde2fbee09e261'
+OurgoodsSplash::Application.config.secret_key_base = ENV['SECRET_KEY_BASE'] || '423b7172dcd2a216dc52fe46f13d618d66c79129f256afd81d6b31fb68ca526a005576a36e9ce4d110666667e2ca404ae760bbe04511d14020cde2fbee09e261'
