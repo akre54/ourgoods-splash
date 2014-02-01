a3791f0297b00e3806ecf3dd5e7252e2626f00d2
Adam Krebs <amk528@cs.nyu.edu>
2014-02-01 15:34:03 +0200

    add datetime order to active event scope

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ActiveRecord::Base
   has_many :signups
 
-  scope :active, lambda { where("event_begin_time >= ?", Time.now) }
+  scope :active, lambda { where("event_begin_time >= ?", Time.now).order('event_begin_time asc') }
 end
