ad0548486c9832c89f3d544e70354992fa30b6b0
Adam Krebs <amk528@cs.nyu.edu>
2014-02-21 14:02:06 -0500

    add timezone-awareness to Event.active

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ActiveRecord::Base
   has_many :signups
 
-  scope :active, lambda { where("event_begin_time >= ?", Time.now).order('event_begin_time asc') }
+  scope :active, lambda { where("event_begin_time >= ?", Time.zone.now).order('event_begin_time asc') }
 end
