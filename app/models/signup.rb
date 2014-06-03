a85cf80f065741208939f610c164c83ab5fe0fe2
Adam Krebs <amk528@cs.nyu.edu>
2014-06-02 22:20:54 -0400

    re-add deprecated fields

--- app/models/signup.rb
+++ app/models/signup.rb
@@ -1,6 +1,10 @@
 class Signup < ActiveRecord::Base
   belongs_to :event
 
+  # deprecated
+  belongs_to :have,     class_name: "Barterable", foreign_key: "have_id"
+  belongs_to :need,     class_name: "Barterable", foreign_key: "need_id"
+
   belongs_to :item,      class_name: "Barterable", foreign_key: "item_id"
   belongs_to :skill,     class_name: "Barterable", foreign_key: "skill_id"
 
